f13f439053009321f764b1e83c251811e85f6b2c
Sebastian Serrano <serranosebastian23@csu.fullerton.edu>
2023-03-04 11:39:48 -0800

    added the core app folder, put a model in there

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """
+    A topic the user is learning About.
+    """
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Return a string reprensation of the model
+        """
+        return self.text
