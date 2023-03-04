194fff02140a9be6c5f9daacb5472a150c9bfcd6
Sebastian Serrano <serranosebastian23@csu.fullerton.edu>
2023-03-04 12:05:42 -0800

    registered the model in the admin file

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
+from site import ENABLE_USER_SITE
 from django.contrib import admin
-from .models import Topic
+# accessing the model file and the classes
+from .models import Topic, Entry
 # Register your models here.
 admin.site.register(Topic)
+admin.sire.register(Entry)
