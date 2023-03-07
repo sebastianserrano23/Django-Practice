04a1586904da83a296722ef98210bd5decb3bbd7
Sebastian Serrano <serranosebastian23@gmail.com>
2023-03-06 21:44:01 -0800

    wrote a line of code that is slowly the beginning to making the home page to the website

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    The home page for learning logs.
+    """
+    return render(request, 'learning_logs/index.html')
